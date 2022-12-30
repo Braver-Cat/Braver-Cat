ade66f7e6bf2c70f25d07346403393a63cc03e8e
Daniele Solombrino <daniele.solombrino@gmail.com>
2022-12-30 10:53:45 +0100

    make __get_item__ return patch_metadata as well

--- src/BRATS2013Dataset.py
+++ src/BRATS2013Dataset.py
@@ -3,11 +3,16 @@ from torch.utils.data import Dataset
 
 import numpy as np
 
+import json
+
+
 class BRATS2013Dataset(Dataset):
 
-  def __init__(self, obs_list, stage):
+  def __init__(self, obs_list, stage, patch_size):
+    
     self.obs_list = obs_list
     self.stage = stage
+    self.patch_size = patch_size
 
   def __len__(self):
     return len(self.obs_list)
@@ -21,11 +26,31 @@ class BRATS2013Dataset(Dataset):
     obs_id = obs_folder_name[0]
     slice_id = obs_folder_name[1]
 
+    patch_metadata = self._load_json(
+      f"{self.obs_list[idx]}/seg_label_to_patch_id_patch_size_{self.patch_size}.json"
+    )
+
     return {
+      "full_path": self.obs_list[idx],
+
       "img": img,
       "label": label,
+      
       "obs_id": obs_id,
       "slice_id": slice_id,
-      "full_path": self.obs_list[idx],
+      
+      "patch_size": self.patch_size,
+      "patch_metadata": patch_metadata,
+      
       "stage": self.stage
     }
+
+  def _load_json(self, json_path):
+  
+    json_file = open(json_path)
+      
+    json_data = json.load(json_file)
+      
+    json_file.close()
+
+    return json_data
