4be13335c8096eca425e75d5a11a99de3bd59c0d
Daniele Solombrino <solombrino.1743111@studenti.uniroma1.it>
2023-01-21 10:47:35 +0100

    [WIP] prepare datasets in main script

--- src/BRATS2013DatasetPatch.py
+++ src/BRATS2013DatasetPatch.py
@@ -1,24 +1,36 @@
 import torch
 from torch.utils.data import Dataset
 
-import polars as pls
+import pandas as pd
 
 import numpy as np
 
 class BRATS2013DatasetPatch(Dataset):
 
-  def __init__(self, patch_df, patch_size, stage):
+  def __init__(self, patch_df_path, patch_size, stage):
     
-    self.patch_df = patch_df
+    self.patch_df = self._load_df(patch_df_path)
     self.patch_size = patch_size
+    self.patch_column_name = f"patch_{self.patch_size}_x_{self.patch_size}_img_path"
 
     self.stage = stage
 
   def __len__(self):
-    return len(self.patch_df)
+    return len(self.patch_df.index)
 
   def __getitem__(self, idx):
+
+    patch = np.load(
+      f"{self.patch_df.iloc[idx][self.patch_column_name]}.npy"
+    )
+    patch_label = self.patch_df.iloc[idx]["patch_label"]
+
+    return {
+      "patch": patch,
+      "patch_label": patch_label
+    }
     
-    return -123
-
-
+  
+  def _load_df(self, df_path):
+    return pd.read_json(df_path)
+  
--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ import argparse
 
 from BRATS2013DatasetPatch import BRATS2013DatasetPatch
 
+from tqdm import tqdm
+
 def parse_cli_args():
   
   arg_parser = argparse.ArgumentParser()
@@ -13,14 +15,53 @@ def parse_cli_args():
     type=str, required=True,
     help="Path of DataFrame storing the input-output tuples"
   )
+  arg_parser.add_argument(
+    "-s", "--patch-size", action="store", dest="patch_size",
+    type=str, required=True,
+    help="Size of the patch to center around the pixel that must be classified for the segmentation"
+  )
 
   parsed_args = arg_parser.parse_args()
 
   return parsed_args
 
+def check_patch_size_consistency(patch_size, dataset_df_path):
+
+  if patch_size not in dataset_df_path:
+    print(
+      f"Patch size {patch_size} not present in patch dataframe path: {dataset_df_path}"
+    )
+
+    return False
+  
+  return True
+
 
 def main():
   parsed_args = parse_cli_args()
+
+  patch_size_consistency_result = check_patch_size_consistency(
+    patch_size=parsed_args.patch_size, 
+    dataset_df_path=parsed_args.dataset_df_path
+  )
+
+  if not patch_size_consistency_result:
+    exit(patch_size_consistency_result)
+
+  dataset_train_path = f"{parsed_args.dataset_df_path}/train_labels_df.json"
+  dataset_val_path = f"{parsed_args.dataset_df_path}/val_labels_df.json"
+  dataset_test_path = f"{parsed_args.dataset_df_path}/test_labels_df.json"
+
+  dataset_train = BRATS2013DatasetPatch(
+    patch_df_path=dataset_train_path, patch_size= parsed_args.patch_size, 
+    stage="train"
+  )
+
+  for x in tqdm(dataset_train):
+    pass
+
+
+
 
 
 if __name__ == "__main__":
