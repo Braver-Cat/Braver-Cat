a5c1060aed3511fb02eea865fbeaab89597118d8
Daniele Solombrino <solombrino.1743111@studenti.uniroma1.it>
2023-01-21 12:03:32 +0100

    [DONE] prepare dataloaders in main script

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-import torch
-
 import argparse
 
 from BRATS2013DatasetPatch import BRATS2013DatasetPatch
 
 from tqdm import tqdm
+
+from torch.utils.data import DataLoader
 
 def parse_cli_args():
   
@@ -24,6 +24,15 @@ def parse_cli_args():
     "-l", "--load-data-in-memory", action="store_true", dest="load_data_in_memory",
     help="Whether to load the entire dataset in memory, rather than loading batch elements when needed in the forward pass"
   )
+  arg_parser.add_argument(
+    "-b", "--batch-size", action="store", dest="batch_size",
+    type=int, required=True,
+    help="Batch size"
+  )
+  arg_parser.add_argument(
+    "-d", "--deterministic", action="store_true", dest="deterministic",
+    help="Whether to use a deterministic behaviour, wherever possible"
+  )
 
   parsed_args = arg_parser.parse_args()
 
@@ -40,18 +49,7 @@ def check_patch_size_consistency(patch_size, dataset_df_path):
   
   return True
 
-
-def main():
-  parsed_args = parse_cli_args()
-
-  patch_size_consistency_result = check_patch_size_consistency(
-    patch_size=parsed_args.patch_size, 
-    dataset_df_path=parsed_args.dataset_df_path
-  )
-
-  if not patch_size_consistency_result:
-    exit(patch_size_consistency_result)
-
+def get_datasets(parsed_args):
   dataset_train_path = f"{parsed_args.dataset_df_path}/train_labels_df.json"
   dataset_val_path = f"{parsed_args.dataset_df_path}/val_labels_df.json"
   dataset_test_path = f"{parsed_args.dataset_df_path}/test_labels_df.json"
@@ -67,6 +65,42 @@ def main():
   dataset_test = BRATS2013DatasetPatch(
     patch_df_path=dataset_test_path, patch_size=parsed_args.patch_size, 
     load_in_memory=parsed_args.load_data_in_memory, stage="test"
+  )
+
+  return dataset_train, dataset_val, dataset_test
+
+def get_dataloaders(dataset_train, dataset_val, dataset_test, parsed_args):
+  dl_train = DataLoader(
+    dataset=dataset_train, batch_size=parsed_args.batch_size, 
+    shuffle=True, num_workers=16
+  )
+  dl_val = DataLoader(
+    dataset=dataset_val, batch_size=parsed_args.batch_size, 
+    shuffle=True, num_workers=16
+  )
+  dl_test = DataLoader(
+    dataset=dataset_test, batch_size=parsed_args.batch_size, 
+    shuffle=True, num_workers=16
+  )
+
+  return dl_train, dl_val, dl_test
+
+
+def main():
+  parsed_args = parse_cli_args()
+
+  patch_size_consistency_result = check_patch_size_consistency(
+    patch_size=parsed_args.patch_size, 
+    dataset_df_path=parsed_args.dataset_df_path
+  )
+
+  if not patch_size_consistency_result:
+    exit(patch_size_consistency_result)
+
+  dataset_train, dataset_val, dataset_test = get_datasets(parsed_args)
+
+  dl_train, dl_val, dl_test = get_dataloaders(
+    dataset_train, dataset_val, dataset_test, parsed_args
   )
 
 
