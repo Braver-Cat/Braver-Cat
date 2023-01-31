6434c6708364620f1316b7bd08f974f9492bd71f
Daniele Solombrino <solombrino.1743111@studenti.uniroma1.it>
2023-01-31 16:22:09 +0100

    remove unused code

--- src/WandBHelper.py
+++ src/WandBHelper.py
@@ -57,31 +57,3 @@ class WandBHelper():
         "train/lr": learning_rate
       }
     )
-
-  
-
-
-
-
-
-
-  
-  # def set_wandb_config(self, parsed_args):
-
-  #   wandb.config = {
-  #     "dataset_df_path": parsed_args.dataset_df_path,
-  #     "patch_size": parsed_args.patch_size,
-  #     "batch_size": parsed_args.batch_size,
-  #     "cascade_type": parsed_args.cascade_type,
-  #     "optimizer": parsed_args.optimizer_name,
-  #     "momentum": parsed_args.momentum,
-  #     "learning_rate": parsed_args.learning_rate,
-  #     "learning_rate_decay_factor": parsed_args.learning_rate_decay_factor,
-  #     "learning_rate_decay_step_size": parsed_args.learning_rate_decay_step_size,
-  #     "num_batches": parsed_args.num_batches,
-  #     "delta_1": parsed_args.delta_1,
-  #     "delta_2": parsed_args.delta_2,
-  #     "dropout": parsed_args.dropout,
-  #     "num_epochs": parsed_args.num_epochs,
-  #   }
-
