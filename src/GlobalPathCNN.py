ea2f5419b7571374ecd927d93e192bd931d0f098
Daniele Solombrino <solombrino.1743111@studenti.uniroma1.it>
2023-03-10 19:26:42 +0100

    add GlobalPathCNN

---
+++ src/GlobalPathCNN.py
@@ -0,0 +1,25 @@
+from torch import nn
+import torch
+
+from MaxOutLayer import MaxOutLayer
+
+class GlobalPathCNN(nn.Module):
+  
+  def __init__(self, in_channels, out_channels, dropout_p):
+
+    super().__init__()
+
+    self.maxout_layer = MaxOutLayer(
+      in_channels=in_channels, out_channels=out_channels, kernel_size=13
+    )
+
+    self.dropout = torch.nn.Dropout2d(p=dropout_p)
+
+  
+  def forward(self, x):
+
+    x = self.maxout_layer(x)
+
+    x = self.dropout(x)
+
+    return x
