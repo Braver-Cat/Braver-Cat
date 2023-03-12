afe5eb606fb268f70f7fcae770e656edfe3e865a
Daniele Solombrino <solombrino.1743111@studenti.uniroma1.it>
2023-03-12 10:12:46 +0100

    hardcode out_channels for Global and Local Path CNNs, as per paper specs

--- src/GlobalPathCNN.py
+++ src/GlobalPathCNN.py
@@ -5,12 +5,12 @@ from MaxOutLayer import MaxOutLayer
 
 class GlobalPathCNN(nn.Module):
   
-  def __init__(self, in_channels, out_channels, dropout_p):
+  def __init__(self, in_channels, dropout_p):
 
     super().__init__()
 
     self.maxout_layer = MaxOutLayer(
-      in_channels=in_channels, out_channels=out_channels, kernel_size=13
+      in_channels=in_channels, out_channels=21, kernel_size=13
     )
 
     self.dropout = torch.nn.Dropout2d(p=dropout_p)
--- src/LocalPathCNN.py
+++ src/LocalPathCNN.py
@@ -5,7 +5,7 @@ from MaxOutLayer import MaxOutLayer
 
 class LocalPathCNN(nn.Module):
   
-  def __init__(self, in_channels, out_channels, dropout_p):
+  def __init__(self, in_channels, dropout_p):
 
     super().__init__()
 
@@ -16,7 +16,7 @@ class LocalPathCNN(nn.Module):
     self.max_pool_0 = nn.MaxPool2d(kernel_size=4, stride=1)
     
     self.maxout_layer_1 = MaxOutLayer(
-      in_channels=64, out_channels=out_channels, kernel_size=3
+      in_channels=64, out_channels=21, kernel_size=3
     )
 
     self.max_pool_1 = nn.MaxPool2d(kernel_size=2, stride=1)
